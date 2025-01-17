0ea1e2c1d22a7cd20043f67a9cbe0cf0ce454ed5
soyuz-dev <avon2ramesh@gmail.com>
2025-01-17 13:57:11 +0800

    Updated Vector.java
    Updated constructors to use getters and setters

--- src/Vector.java
+++ src/Vector.java
@@ -21,47 +21,47 @@ public class Vector {
 
     public static Vector sum(Vector lh, Vector rh){
         Rect cur = new Rect(0,0);
-        cur.x = lh.recform.x + rh.recform.x;
-        cur.y = lh.recform.y + rh.recform.y;
+        cur.setX(lh.recform.getX() + rh.recform.getX());
+        cur.setY(lh.recform.getY() + rh.recform.getY());
         return new Vector(cur);
     }
 
     public static Vector minus(Vector lh, Vector rh){
         Rect cur = new Rect(0,0);
-        cur.x = lh.recform.x - rh.recform.x;
-        cur.y = lh.recform.y - rh.recform.y;
+        cur.setX(lh.recform.getX() - rh.recform.getX());
+        cur.setY(lh.recform.getY() - rh.recform.getY());
         return new Vector(cur);
     }
 
     public static Vector difference(Vector lh, Vector rh){
         Rect cur = new Rect(0,0);
-        cur.x = abs(lh.recform.x - rh.recform.x);
-        cur.y = abs(lh.recform.y - rh.recform.y);
+        cur.setX(abs(lh.recform.getX() - rh.recform.getX()));
+        cur.setY(abs(lh.recform.getY() - rh.recform.getY()));
         return new Vector(cur);
     }
 
     public void add(Vector other){
-        this.recform.x += other.recform.x;
-        this.recform.y += other.recform.y;
+        this.recform.setX(this.recform.getX() + other.recform.getX());
+        this.recform.setY(this.recform.getY() + other.recform.getY());
         this.polform = Polar.pol(this.recform);
     }
 
     public void subtract(Vector other){
-        this.recform.x -= other.recform.x;
-        this.recform.y -= other.recform.y;
+        this.recform.setX(this.recform.getX() - other.recform.getX());
+        this.recform.setY(this.recform.getY() - other.recform.getY());
         this.polform = Polar.pol(this.recform);
     }
 
     public static void printRec(Vector x){
-        System.out.println(String.format("X: %.5f, Y: %.5f",x.recform.x,x.recform.y));
+        System.out.println(String.format("X: %.5f, Y: %.5f",x.recform.getX(),x.recform.getY()));
     }
 
     public static void printPol(Vector x){
-        System.out.println(String.format("Magnitude: %.5f, Direction: %.5f radians",x.polform.mag,x.polform.theta));
+        System.out.println(String.format("Magnitude: %.5f, Direction: %.5f radians",x.polform.getMag(),x.polform.getTheta()));
     }
 
     public static void printPolDeg(Vector x){
-        System.out.println(String.format("Magnitude: %.5f, Direction: %.5f degrees",x.polform.mag,Angle.deg(x.polform.theta)));
+        System.out.println(String.format("Magnitude: %.5f, Direction: %.5f degrees",x.polform.mag,Angle.deg(x.polform.getTheta())));
     }
 
     public static void print(Vector x){
