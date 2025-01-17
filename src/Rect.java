127207ef1fc8edb4ec2645ec767d5701da8e2fc5
soyuz-dev <avon2ramesh@gmail.com>
2025-01-17 13:42:44 +0800

    Updated Rect.java
    -Added Setters and Getters for x and y
    -updated Rect(Polar x) constructor to use setters and getters

--- src/Rect.java
+++ src/Rect.java
@@ -1,22 +1,38 @@
 import static java.lang.Math.sqrt;
 
 public class Rect {
-    public double x;
-    public double y;
-
+    private double x;
+    private double y;
+    
     public Rect(double x, double y){
         this.x = x;
         this.y = y;
     }
 
+    public void setX(double val){
+        this.x = val;
+    }
+    
+    public void setY(double val){
+        this.y = val;
+    }
+    
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+    
     public double getMagnitude(){
         return sqrt(x*x + y*y);
     }
 
     public static Rect rec(Polar x){
         Rect cur = new Rect(0,0);
-        cur.x = Math.cos(x.theta)*x.mag;
-        cur.y = Math.sin(x.theta)*x.mag;
+        cur.x = Math.cos(x.getTheta())*x.getMag();
+        cur.y = Math.sin(x.getTheta())*x.getMag();
         return cur;
     }
 }
