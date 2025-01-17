09fc2f7307f528df9c7be08af7f02d9572a47d53
Soyuz <avon2ramesh@gmail.com>
2025-01-17 21:06:05 +0800

    Updated Point.java
    Added functionality to the Point class

--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,23 @@
-public class Point extends Rect {
+public class Point {
+    private double x, y;
+    private Rect coords;
     
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+        this.coords = new Rect(x,y);
+        
+    }
+    
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+        this.coords = new Rect(x,y);
+    }
+
+    public void setCoords(double x, double y){
+        this.x = x;
+        this.y = y;
+        this.coords.setCoords(this.x, this.y);
+    }
 }
