5345e3f0abf375d2f4e814aa3bbde130d7071368
Soyuz-dev <avon2ramesh@gmail.com>
2025-01-23 12:10:46 +0800

    Added constructors for Displacement.java

--- src/Displacement.java
+++ src/Displacement.java
@@ -1,2 +1,15 @@
 public class Displacement extends Vector{
+    public Displacement(double a, double b, double c, double d) {
+        super(a, b, c, d);
+    }
+
+    public Displacement(Polar x) {
+        super(x);
+    }
+
+    public Displacement(Rect x) {
+        super(x);
+    }
+
+
 }
