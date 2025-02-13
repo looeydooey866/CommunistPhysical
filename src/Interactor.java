2168ffb41794e7640f5874d72f748814ac2c34c4
looeydooey866 <looeydooey866@gmail.com>
2025-02-13 09:41:28 +0800

    File separator added to automatically adjust to OS.

--- src/Interactor.java
+++ src/Interactor.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -123,7 +124,7 @@ public class Interactor {
     }
 
     private void pyplot() throws Exception { // please rehaul
-        String pythonFileName = "src/grapher.py";
+        String pythonFileName = String.format("src%sgrapher.py", File.separator);
 
         ArrayList<String> args = new ArrayList<>();
         ArrayList<String> names = new ArrayList<>();
